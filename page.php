f9354a27f0eab98010e2234ecbbd7fbeb998542f
stomen <ivan.krasovskij@gmail.com>
2017-02-19 00:15:22 +0200

    upl princ

--- page.php
+++ page.php
@@ -36,5 +36,50 @@
 			</div>
 		</div>
 	</div>
+	<div class="our-principles">
+		<div class="center">
+			<div class="title-block">
+				<span>Наши принципы</span>
+			</div>
+			<div class="our-principles-block">
+				<div class="our-principles-items our-principles-item1">
+					<span>Рэджио философия и педагогика слушания</span>
+					<span>Мы воплощаем в жизнь самый прогрессивный подход к дошкольному образованию Reggio Emilia</span>
+				</div>
+				<div class="our-principles-items our-principles-item2">
+					<span>Игровое обучение на английском</span>
+					<span>Мы учимся, играя, потому что, знаем о большом потенциале креативной игры.</span>
+				</div>
+				<div class="our-principles-items our-principles-item3">
+					<span>Эко-пространство</span>
+					<span>Как и в Монтессори, мы убеждены в том, что пространство является третьим учителем.</span>
+				</div>
+				<div class="our-principles-items our-principles-item4">
+					<span>100 языков детей</span>
+					<span>Мы создаем возможности для свободного творчества.</span>
+				</div>
+				<div class="our-principles-items our-principles-item5">
+					<span>Подготовка к школе будущего</span>
+					<span>Мы развиваем навыки XXI века, креативность и воображение.</span>
+				</div>
+				<div class="our-principles-items our-principles-item6">
+					<span>Индивидуальный подход</span>
+					<span>Наши мамы и папы знают, какой опыт получают их дети, что их увлекает и над чем они работали сегодня.</span>
+				</div>
+				<div class="our-principles-items our-principles-item7">
+					<span>Фан</span>
+					<span>Мы превращаем обычные дела в приключения, потому что в детстве должно быть задорно и весело.</span>
+				</div>
+				<div class="our-principles-items our-principles-item8">
+					<span>Теория привязанности</span>
+					<span>Мы прививаем доброту и уважение ко всему и каждому, лелеем доверие и самые теплые семейные ценности. </span>
+				</div>
+				<div class="our-principles-items our-principles-item9">
+					<span>Проекты</span>
+					<span> Мы ведем настоящую проектную деятельность, даем разгуляться детскому любопытству</span>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
 <?php get_footer();?>
